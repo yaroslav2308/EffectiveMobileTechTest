ed1864d2cb9afdafc644c9cc03c4900b1fc52d5b
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-08-24 15:49:06 +0300

    Initial Commit

---
+++ EffectiveMobileTechTest/EffectiveMobileTechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTechTestApp.swift
+//  EffectiveMobileTechTest
+//
+//  Created by Yaroslav Monastyrev on 24.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
